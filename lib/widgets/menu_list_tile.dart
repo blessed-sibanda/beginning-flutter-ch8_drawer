21a1a9e02faa45a5afcec2061bbb04835c650986
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-01 11:07:17 +0200

    create menu-list-tile widget

---
+++ lib/widgets/menu_list_tile.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import '../pages/birthdays.dart';
+import '../pages/reminders.dart';
+import '../pages/gratitude.dart';
+
+class MenuListTile extends StatelessWidget {
+  const MenuListTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: const Icon(Icons.cake),
+          title: const Text('Birthdays'),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const Birthdays(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.sentiment_satisfied),
+          title: const Text('Gratitude'),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const Gratitude(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.access_alarm),
+          title: const Text('Reminders'),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const Reminders(),
+              ),
+            );
+          },
+        ),
+        const Divider(color: Colors.grey),
+        ListTile(
+          leading: const Icon(Icons.settings),
+          title: const Text('Setting'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
